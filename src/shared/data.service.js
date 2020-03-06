130817c13bed5443fb1369a215c99b5636e2f10d
Michael Wybraniec <michaelwybraniec@me.com>
2020-03-06 16:12:34 +0100

    url fix

--- src/shared/data.service.js
+++ src/shared/data.service.js
@@ -2,12 +2,11 @@ import axios from "axios";
 import { API } from "./config";
 
 
-const getHero = async id =>{
-const url = `https://cors-anywhere.herokuapp.com/
-  ${API.heroes.url}/${API.heroes.key}/${id}`;
-axios.get(url)
-.then(response => response.data)
-.catch(error => error);
+const getHero = async id => {
+  const url = `https://cors-anywhere.herokuapp.com/${API.heroes.url}/${API.heroes.key}/${id}`;
+  axios.get(url)
+    .then(response => response.data)
+    .catch(error => error);
 };
 
 
