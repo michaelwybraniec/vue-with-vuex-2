a523f0a11a26a92e503dc474ff148ac2b3abebb5
Michael Wybraniec <46869143+michaelwybraniec@users.noreply.github.com>
2020-03-12 18:15:39 +0100

    init

--- src/views/hero-detail.vue
+++ src/views/hero-detail.vue
@@ -1,179 +1,217 @@
 <template>
-  <b-container>
-    <b-card class="mt-4 mb-4 shadow-sm p-4">
-      <b-row>
-        <b-col>
-          <b-row class="my-1 mb-2 text-center">
-            <b-col>
-              <div style="height: 120px">
-                <div
-                  class="text-center"
-                  style="padding-top: 40px;"
-                  v-if="!picLoaded"
-                  v-show="message"
-                >
-                  <b-spinner type="grow"></b-spinner>
-                  <p>{{ message }}</p>
-                </div>
-                <div v-if="picLoaded">
-                  <b-img :src="pic" v-bind="mainProps" fluid alt="img"></b-img>
-                </div>
+  <b-card class="mt-2 mb-2 m-0 ">
+    <b-row>
+      <b-col>
+        <b-row class="my-1 mb-2 text-center">
+          <b-col>
+            <div style="height: 280px">
+              <div
+                class="text-center"
+                style="padding-top: 40px;"
+                v-if="!picLoaded"
+                v-show="message"
+              >
+                <b-spinner type="grow"></b-spinner>
+                <p>{{ message }}</p>
               </div>
-              <p>
-                <b>{{ clonedHero.name }}</b>
-                : {{ clonedHero.description }}
-              </p>
-              <p>
-                ( ID:
-                <code>{{ clonedHero.id }}</code> )
-              </p>
-            </b-col>
-          </b-row>
-
-          <b-row class="my-1">
-            <b-col sm="2">
-              <label for="name">Name:</label>
-            </b-col>
-            <b-col sm="10">
-              <b-form-input id="name" v-model="clonedHero.name"></b-form-input>
-            </b-col>
-          </b-row>
-
-          <b-row>
-            <b-col sm="2">
-              <label for="description">Description:</label>
-            </b-col>
-            <b-col sm="10">
-              <b-form-input
-                id="description"
-                v-model="clonedHero.description"
-              ></b-form-input>
-            </b-col>
-          </b-row>
-
-          <div v-if="comicses.length > 0">
-            <div class="text-center pt-4">
-              Comics found: {{ clonedHero.comics.items.length }}
+              <div v-if="picLoaded">
+                <b-img :src="pic" v-bind="mainProps" fluid alt="img"></b-img>
+              </div>
             </div>
-            <div class="text-center pt-2 pb-2">First 3:</div>
-            <div :key="comics.name" v-for="(comics, index) in comicses">
-              <b-row class="my-1" v-if="index < 3">
-                <b-col sm="2">
-                  <label>{{ index + 1 }}:</label>
-                </b-col>
-                <b-col sm="10" style="padding-left: 15px"
-                  >{{ comics.name }} ({{ comics.name }})</b-col
+            <h3>{{ clonedHero.name }}</h3>
+            <p>
+              ( ID: <code>{{ clonedHero.id }}</code> )
+            </p>
+          </b-col>
+        </b-row>
+
+        <div
+          v-for="(value, name, index) in clonedHero"
+          v-bind:key="index + name"
+          class=""
+        >
+          <b-row v-if="index !== 0" class="border-bottom mt-2 pb-2">
+            <b-col md="2">
+              <label for="{name}">
+                <b>{{ name }}:</b>
+              </label>
+            </b-col>
+            <b-col md="10">
+              <span v-if="typeof value === 'string'" class="border-left pl-3">
+                {{ value }}
+              </span>
+              <span v-else>
+                <div
+                  v-for="(value, name, index) in value"
+                  v-bind:key="index + name"
+                  class="border-left pl-3"
                 >
-              </b-row>
-            </div>
-          </div>
-
-          <b-row class="text-center">
-            <b-col
-              v-show="messageNoComics"
-              class="m-4"
-              style="padding-top: 57px;"
-            >
-              <code>{{ messageNoComics }}</code>
-            </b-col>
-
-            <b-col
-              v-show="message4comics"
-              class="m-4"
-              style="padding-top: 20px;"
-            >
-              <b-spinner type="grow"></b-spinner>
-              <p>{{ message4comics }}</p>
+                  <b-row>
+                    <b-col md="3">
+                      <b>{{ name }}:</b>
+                    </b-col>
+                    <b-col md="9">
+                      <div
+                        v-if="
+                          typeof value === 'string' || typeof value === 'number'
+                        "
+                        class="border-left pl-3"
+                      >
+                        {{ value }}
+                      </div>
+                      <span v-else>
+                        <div
+                          v-for="(value, name, index) in value"
+                          v-bind:key="index + value"
+                          class="border-left pl-3"
+                        >
+                          <span v-if="typeof value === 'string'">
+                            {{ value }}
+                          </span>
+                        </div>
+                      </span>
+                    </b-col>
+                  </b-row>
+                </div>
+              </span>
             </b-col>
           </b-row>
+        </div>
 
-          <div class="float-right mt-4">
-            <b-button class="mr-2" variant="light" @click="cancelHero()"
-              >Cancel</b-button
-            >
-            <b-button variant="light" @click="saveHero()">Save</b-button>
+        <!-- <b-row class="my-1">
+          <b-col md="2">
+            <label for="name">Name:</label>
+          </b-col>
+          <b-col md="10">
+            <b-form-input id="name" v-model="clonedHero.name"></b-form-input>
+          </b-col>
+        </b-row>
+
+        <b-row>
+          <b-col md="2">
+            <label for="description">Description:</label>
+          </b-col>
+          <b-col md="10">
+            <b-form-input
+              id="description"
+              v-model="clonedHero.description"
+            ></b-form-input>
+          </b-col>
+        </b-row>
+
+        <div v-if="comicses.length > 0">
+          <div class="text-center pt-4">
+            Comics found: {{ clonedHero.comics.items.length }}
           </div>
-        </b-col>
-      </b-row>
-    </b-card>
-  </b-container>
+          <div class="text-center pt-2 pb-2">First 3:</div>
+          <div :key="comics.name" v-for="(comics, index) in comicses">
+            <b-row class="my-1" v-if="index < 3">
+              <b-col md="2">
+                <label>{{ index + 1 }}:</label>
+              </b-col>
+              <b-col md="10" style="padding-left: 15px"
+                >{{ comics.name }} ({{ comics.name }})</b-col
+              >
+            </b-row>
+          </div>
+        </div>
+
+        <b-row class="text-center">
+          <b-col
+            v-show="messageNoComics"
+            class="m-4"
+            style="padding-top: 57px;"
+          >
+            <code>{{ messageNoComics }}</code>
+          </b-col>
+
+          <b-col v-show="message4comics" class="m-4" style="padding-top: 20px;">
+            <b-spinner type="grow"></b-spinner>
+            <p>{{ message4comics }}</p>
+          </b-col>
+        </b-row>
+
+        <div class="float-right mt-4">
+          <b-button class="mr-2" variant="light" @click="cancelHero()"
+            >Cancel</b-button
+          >
+          <b-button variant="light" @click="saveHero()">Save</b-button>
+        </div> -->
+      </b-col>
+    </b-row>
+  </b-card>
 </template>
 
 <script>
 export default {
-  name: "HeroDetail",
+  name: 'HeroDetail',
   props: {
     id: {
       type: Number,
-      default: 0,
+      default: 0
     },
     hero: {
       type: Object,
-      default: () => {},
-    },
+      default: () => {}
+    }
   },
   data() {
     return {
       clonedHero: {},
-      message: "",
-      message4comics: "",
-      messageNoComics: "",
+      message: '',
+      message4comics: '',
+      messageNoComics: '',
       picLoaded: undefined,
-      pic: "",
+      pic: '',
       comicses: [],
       mainProps: {
         center: true,
-        width: 100,
-      },
-    };
+        width: 200
+      }
+    }
   },
   computed: {
     fullName() {
       return this.clonedHero
         ? this.clonedHero.name + this.clonedHero.description
-        : "";
-    },
+        : ''
+    }
   },
   async mounted() {
-    await this.cloneHero();
-    await this.loadPics();
-    await this.loadComics();
+    await this.cloneHero()
+    await this.loadPics()
+    await this.loadComics()
   },
   methods: {
     async cloneHero() {
-      this.clonedHero = { ...this.hero }; // to avoid obj mutation.
+      this.clonedHero = { ...this.hero } // to avoid obj mutation.
     },
     async loadPics() {
-      this.message = "Loading photo...";
-      this.pic =
-        this.clonedHero.thumbnail &&
-        this.clonedHero.thumbnail.path +
-          "." +
-          this.clonedHero.thumbnail.extension;
+      this.message = 'Loading photo...'
+      this.pic = this.clonedHero.image && this.clonedHero.image.url
 
       if (this.pic) {
-        this.picLoaded = true;
-        this.message = "";
-      } else this.message = "Err.";
+        this.picLoaded = true
+        this.message = ''
+      } else this.message = 'Err.'
     },
     loadComics() {
-      this.message4comics = "And now getting comics info...";
-      this.comicses = this.clonedHero.comics.items;
-      console.log("All comics available:", this.clonedHero.comics.items.length);
+      this.message4comics = 'And now getting comics info...'
+      this.comicses = this.clonedHero.comics.items
+      console.log('All comics available:', this.clonedHero.comics.items.length)
       if (this.clonedHero.comics.items.length === 0) {
-        this.messageNoComics = "No commics available :-(";
+        this.messageNoComics = 'No commics available :-('
       } else {
-        this.messageNoComics = "";
+        this.messageNoComics = ''
       }
-      this.message4comics = "";
+      this.message4comics = ''
     },
     cancelHero() {
-      this.$emit("cancel");
+      this.$emit('cancel')
     },
     async saveHero() {
-      this.$emit("save", this.clonedHero);
-    },
-  },
-};
+      this.$emit('save', this.clonedHero)
+    }
+  }
+}
 </script>
-
--- src/views/heroes.vue
+++ src/views/heroes.vue
@@ -1,188 +1,219 @@
 <template>
   <b-container>
-      <b-card-group deck class="pt-4">
-        <b-card header="Heroes" class="shadow">
-          <b-row>
-            <b-col>
-              <div >
-                <div>
-                  <b-form inline>
-                    <label class="sr-only" for="inline-form-input-name">Search:</label>
+    <b-card-group deck class="pt-4">
+      <b-card header="Heroes" class="shadow">
+        <b-row>
+          <b-col>
+            <div>
+              <div>
+                <b-row class="mb-2">
+                  <b-col sm="6" class="pr-2">
+                    <label class="sr-only" for="inline-form-input-name"
+                      >Search:</label
+                    >
                     <b-input
                       id="inline-form-input-name"
-                      class="mb-2 mr-sm-2 mb-sm-0"
+                      class="mb-20"
                       placeholder="Name or ID"
                       v-model="search.input"
                       v-on:keyup.enter="getHero()"
                     ></b-input>
+                  </b-col>
 
-                    <b-button
-                      variant="light"
-                      class="border"
-                      @click="getHero()"
-                      :disabled="this.buttons.search.disabled">
-                     
-                       <div v-if="!loading">
-                          Search
-                       </div>
-                       <div v-else>
-                          <b-spinner small type="grow"></b-spinner>
-                        Loading...
-                       </div>
-                    </b-button>
-
-                    <div v-if="buttons.clear.show && !this.message.error && !this.loading">
-                      <b-button 
-                        variant="light" 
-                        @click="clearHero()"
-                        class="ml-2 border"
+                  <b-col sm="6" class="pl-2">
+                    <div>
+                      <span v-if="!this.message.error">
+                        <b-button
+                          variant="light"
+                          class="border"
+                          @click="getHero()"
+                          :disabled="this.buttons.search.disabled"
                         >
-                        Clear
-                      </b-button>
-                    </div>
+                          <span v-if="!loading">
+                            Search
+                          </span>
+                          <div v-else>
+                            <b-spinner small type="grow"></b-spinner>
+                            {{ this.message.info }}
+                          </div>
+                        </b-button>
+                      </span>
 
-                   <div v-if="this.message.error" >
-                    <b-alert 
-                      show
-                      variant="danger"
-                      style="padding: 6px; margin: 0px;" 
-                      class="ml-2 pl-3 pr-3"
+                      <span
+                        v-if="
+                          buttons.clear.show &&
+                            !this.message.error &&
+                            !this.loading
+                        "
                       >
-                      {{this.message.error}}
-                    </b-alert>
-                  </div>
-                   
-                  </b-form>
-                </div>
-              </div>
+                        <b-button
+                          variant="light"
+                          @click="clearHero()"
+                          class="ml-2 border"
+                        >
+                          Clear
+                        </b-button>
+                      </span>
 
-             
-              <div v-if="!loading">
-
-                <b-list-group v-if="hero">
-                  <div v-if="hero.response !== 'success'">
-                    <div v-for="(value, name, index) in hero" v-bind:key="index">
-                        <b-list-group-item button class="shadow-sm mt-1">
-                        //TODO: Component
-                          <span v-if="value.length">{{value}}</span>
-                            <div v-else>
-                              <div v-for="(value, name, index) in value" v-bind:key="index">
-                                <span> - <i> {{ name }} </i>: {{value}}</span>
-                              </div>
-                            </div>
-                          <br>
-                        </b-list-group-item>
+                      <div v-if="this.message.error">
+                        <b-alert
+                          show
+                          variant="danger"
+                          style="padding: 6px; margin: 0px;"
+                          class="text-center"
+                        >
+                          {{ this.message.error }}
+                        </b-alert>
+                      </div>
                     </div>
-                  </div>
-                  <div v-else>
-                    <b-list-group-item button class="shadow-sm mt-1">
-                      Result: {{hero.name}} <br>
-                      //TODO: Component
+                  </b-col>
+                </b-row>
+              </div>
+            </div>
+
+            <div v-if="!loading">
+              <b-list-group v-if="hero">
+                <div v-if="hero.response !== 'success'">
+                  <div v-for="(value, name, index) in hero" v-bind:key="index">
+                    <b-list-group-item
+                      button
+                      class=""
+                      @click="selectHero(index)"
+                    >
+                      <span v-if="value.length">{{ value }}</span>
+                      <div v-else>
+                        <div
+                          v-for="(value, name, index) in value"
+                          v-bind:key="index"
+                        >
+                          <span>
+                            - <i> {{ name }} </i>: {{ value }}</span
+                          >
+                        </div>
+                      </div>
+                      <br />
                     </b-list-group-item>
                   </div>
-                </b-list-group>
-                
-                <!-- 
-                <HeroDetail
-                  :hero="selectedHero"
-                  @save="saveHero"
-                  @cancel="cancelHero"
-                  v-if="selectedHero"
-                /> -->
+                </div>
+                <div v-else>
+                  <b-list-group-item
+                    button
+                    class="shadow-sm mt-1"
+                    @click="selectHero(hero)"
+                  >
+                    {{ hero.name }}
 
-              </div>
-              
-            </b-col>
-          </b-row>
-        </b-card>
-      </b-card-group>
+                    <HeroDetail
+                      :hero="selectedHero"
+                      @save="saveHero"
+                      @cancel="cancelHero"
+                      v-if="selectedHero"
+                    />
+                  </b-list-group-item>
+                </div>
+              </b-list-group>
+            </div>
+          </b-col>
+        </b-row>
+      </b-card>
+    </b-card-group>
   </b-container>
 </template>
 
 <script>
 //import { mapActions, mapGetters } from "vuex";
 import store from '@/store/index'
-// import HeroDetail from "@/views/hero-detail";
+import HeroDetail from '@/views/hero-detail'
 export default {
-  name: "Heroes",
+  name: 'Heroes',
   data() {
     return {
       loading: false,
+      selectedHero: undefined,
       buttons: {
-        search:{ 
+        search: {
           disabled: true
-          },
+        },
         clear: {
           show: false
         }
       },
       message: {
-        info:undefined,
-        error:undefined,
+        info: undefined,
+        error: undefined,
         success: undefined
       },
       search: {
         input: undefined
-      },
-    };
+      }
+    }
   },
-    components: {
-   // HeroDetail,
+  components: {
+    HeroDetail
   },
   watch: {
-    disableSearch () {
-      if(this.search.input === "") this.buttons.search.disabled = true;
-      else this.buttons.search.disabled = false;
+    disableSearch() {
+      if (this.search.input === '') {
+        this.buttons.search.disabled = true
+        this.buttons.clear.show = false
+        this.message.error = undefined
+      } else {
+        this.buttons.search.disabled = false
+      }
     },
     apiError(errNew, errOld) {
-      console.log("apiError -> errNew, errOld", errNew, errOld)
-      switch(errNew){
-        case "access denied":
-          this.message.error =  "Api access denied, try later pls !";
-          break;
-        case "character with given name not found":
-          // err is falling to the 2nd test on the action level, refactor function.
-          this.message.error = "Hero with a given name not found :-(";
-          break;
-        default: this.message.error = errNew;
+      console.log('apiError -> errNew, errOld', errNew, errOld)
+      switch (errNew) {
+        case 'access denied':
+          this.message.error = 'Api access denied, try later pls !'
+          break
+        case 'character with given name not found':
+          //! err is falling to the 2nd test on the action level, refactor function.
+          this.message.error = 'Hero with a given id or name not found :-('
+          break
+        default:
+          this.message.error = errNew
       }
     }
   },
   computed: {
     disableSearch() {
       return this.search.input
-      },
+    },
     hero() {
       return store.getters.getAvailableHero
     },
-    apiError() { 
+    apiError() {
       return store.getters.getApiErrorMsg
     }
   },
   methods: {
-     async getHero() {
-      if(this.search.input !== ''){
-        this.message.info = "Getting the hero, please be patient!";
-        this.loading = true;
-        this.buttons.search.disabled = true;
+    async getHero() {
+      if (this.search.input !== '') {
+        this.message.info = 'Loading...'
+        this.loading = true
+        this.buttons.search.disabled = true
         await store.dispatch('getHeroAction', this.search.input)
-        this.message.info = undefined;
-        this.loading = false;
-        this.buttons.search.disabled = false;
-        this.buttons.clear.show = true;
-        this.hero ? this.message.error = undefined : null
-
-      }
-      else {
+        this.message.info = undefined
+        this.loading = false
+        this.buttons.search.disabled = false
+        this.buttons.clear.show = true
+        this.hero ? (this.message.error = undefined) : null
+      } else {
         this.message.error = "Input empty, I can't read minds yet!"
-       
       }
     },
-    clearHero(){
+    clearHero() {
       store.dispatch('clearHeroAction')
-      this.buttons.clear.show = false;
-      this.message.error = undefined;
+      this.buttons.clear.show = false
+      this.message.error = undefined
+      this.search.input = undefined
+      this.selectedHero = undefined
+    },
+    selectHero(hero) {
+      console.log('selectHero -> hero', hero)
+      this.selectedHero = hero
     }
-  },
-};
+  }
+}
 </script>
